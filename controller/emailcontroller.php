f7f5f678a633aaf7b4921dc8aa5f541549fc6b06
spiderbbc <spiderbbc@gmail.com>
2016-08-09 21:20:34 -0400

    Simple pruebas con SwiftMailer y Mandrill(Sucessgit add .)

--- controller/emailcontroller.php
+++ controller/emailcontroller.php
@@ -5,7 +5,7 @@ require_once 'vendor/swiftmailer/swiftmailer/lib/swift_init.php';
 
 
 // Create the mail transport configuration
-$transport = Swift_MailTransport::newInstance();
+/*$transport = Swift_MailTransport::newInstance();
 
 // Create the message
 $message = Swift_Message::newInstance();
@@ -20,3 +20,33 @@ $message->setFrom("account@bank.com", "Your bank");
 // Send the email
 $mailer = Swift_Mailer::newInstance($transport);
 $mailer->send($message);
+*/
+
+// Create the Transport
+$transport = Swift_SmtpTransport::newInstance('smtp-pulse.com',2525)
+  ->setUsername('ecastro@quorumtelecom.info')
+  ->setPassword('4Ng4YG92iEk6MK')
+  ;
+
+/*
+You could alternatively use a different transport such as Sendmail or Mail:
+
+// Sendmail
+$transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+
+// Mail
+$transport = Swift_MailTransport::newInstance();
+*/
+
+// Create the Mailer using your created Transport
+$mailer = Swift_Mailer::newInstance($transport);
+
+// Create a message
+$message = Swift_Message::newInstance('test Swift_Mailer y Mandrill')
+  ->setFrom(array('ecastro@quorumtelecom.info' => 'xavier castro'))
+  ->setTo(array('spiderbbc@gmail.com', 'other@domain.org' => 'Qtelecom'))
+  ->setBody('Here is the message itself')
+  ;
+
+// Send the message
+$result = $mailer->send($message);
